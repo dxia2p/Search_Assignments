f582e56eee0b307083ff3288688a2dae5614b446
dxia2p <d.xia111@gmail.com>
2024-02-28 09:50:55 -0700

    added comments

--- Spell Check Assignment/spellcheck.py
+++ Spell Check Assignment/spellcheck.py
@@ -2,7 +2,7 @@
 # This start code creates two lists
 # 1: dictionary: a list containing all of the words from "dictionary.txt"
 # 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
-
+import time
 import re  # Needed for splitting text with a regular expression
 
 def linearSearch(anArray, item):
@@ -43,27 +43,45 @@ def main():
     print(aliceWords[0:50])
 
     inp = printMainMenu()
-    while inp != 5:
+    while inp != 5: # keep printing the menu until the user inputs "5"
         
         match inp:
             case 1:
-                inputWord = input("Please enter a word: ")
+                inputWord = input("Please enter a word: ").lower() # input
                 print("Linear Search Starting...")
-                res = linearSearch(dictionary, inputWord)
-                if res == -1:
+                res = linearSearch(dictionary, inputWord) # process data
+                if res == -1: # output
                     print(f"{inputWord} is NOT IN the dictionary.")
                 else:
                     print(f"{inputWord} is IN the dictionary at position {res}")
             case 2:
-                inputWord = input("Please enter a word: ")
+                inputWord = input("Please enter a word: ").lower() # input
                 print("Linear Search Starting...")
-                res = binarySearch(dictionary, inputWord)
-                if res == -1:
+                res = binarySearch(dictionary, inputWord) # process data
+                if res == -1: # outputs
                     print(f"{inputWord} is NOT IN the dictionary.")
                 else:
                     print(f"{inputWord} is IN the dictionary at position {res}")
             case 3:
-                print("AAAAA")
+                wordsNotFound = 0
+                startTime = time.time() # get the starting time
+                for i in aliceWords: # run linear search for every word
+                    res = linearSearch(dictionary, i.lower())
+                    if res == -1:
+                        wordsNotFound += 1
+                endTime = time.time() # get the ending time
+                timeElapsed = endTime - startTime # calculate the time elapsed
+                print(f"Number of words not found in dictionary: {wordsNotFound} ({timeElapsed} seconds)")
+            case 4:
+                wordsNotFound = 0
+                startTime = time.time() # get the starting time
+                for i in aliceWords: # run linear search for every word
+                    res = binarySearch(dictionary, i.lower())
+                    if res == -1:
+                        wordsNotFound += 1
+                endTime = time.time() # get the ending time
+                timeElapsed = endTime - startTime # calculate the time elapsed
+                print(f"Number of words not found in dictionary: {wordsNotFound} ({timeElapsed} seconds)")
         inp = printMainMenu()
 
 # end main()
